a47e53423f869d4afe9e230227ea7ba285b1d0cf
Sachin-16r <sachinchharwari876426@gmail.com>
2026-09-07 01:48:44 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.js.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI-CHATBOT/', // 👈 यह लाइन जोड़ना अनिवार्य है
 })
